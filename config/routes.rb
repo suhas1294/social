2b0795369e3252f475cdc6f066bcba99bfd817a3
suhas1294 <suhasr94@gmail.com>
2017-04-02 11:51:49 +0530

    friend request feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
  Rails.application.routes.draw do
 
+  resources :friends
   resources :comments
   resources :posts
   resources :dashboard
-  resources :users, only: [:show]
+  # resources :user, only: [:show]
+
+  # friendship routes
+  post "/accept_friend_request/:id" => "friends#accept_friend_request", :as => "accept_friend_request"
+  post "/reject_friend_request/:id" => "friends#reject_friend_request", :as => "reject_friend_request"
+  post "/block_friend_request/:id" => "friends#block_friend_request", :as => "block_friend_request"
+  post "/friend_request/:id" => "friends#create_friendship", :as => "create_friend_request"
+
+
+  get "/user/:id" => "users#show", :as => "show_user_path"
   get "/my_posts" => "dashboard#my_posts", :as => "my_posts"
   devise_for :users
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(first_name: "Suhas", last_name: "Ramesh", username: "suhasr", email: "suhasr@yopmail.com", password: "Password@1", password_confirmation: "Password@1", profile_pic: nil)
+
+User.create(first_name: "Rajnikant", last_name: "Gaikwad", username: "rajnikantg", email: "rajnikantg@yopmail.com", password: "Password@1", password_confirmation: "Password@1", profile_pic: nil)
+
+User.create(first_name: "John", last_name: "Doe", username: "johnd", email: "johnd@yopmail.com", password: "Password@1", password_confirmation: "Password@1", profile_pic: nil)
+
+User.create(first_name: "Amitabh", last_name: "Bacchan", username: "amitabhb", email: "amitabhb@yopmail.com", password: "Password@1", password_confirmation: "Password@1", profile_pic: nil)
+
+User.create(first_name: "Narendra", last_name: "Modi", username: "namo", email: "namo@yopmail.com", password: "Password@1", password_confirmation: "Password@1", profile_pic: nil)
+
+User.create(first_name: "Amit", last_name: "Shah", username: "amits", email: "amits@yopmail.com", password: "Password@1", password_confirmation: "Password@1", profile_pic: nil)
+
+
+Post.create(title: "India", body: "India is a beautiful country", user: User.find_by_email("suhasr@yopmail.com"))
+Post.create(title: "America", body: "America is known for silicon valley", user: User.find_by_email("suhasr@yopmail.com"))
+Post.create(title: "Japan", body: "People are known for puntuality", user: User.find_by_email("suhasr@yopmail.com"))
+Post.create(title: "Australia", body: "Known for Kangroos", user: User.find_by_email("suhasr@yopmail.com"))
+Post.create(title: "srilanka", body: "Good neighbourer of India", user: User.find_by_email("namo@yopmail.com"))
+Post.create(title: "Nepal", body: "Good for mountaineers", user: User.find_by_email("rajnikantg@yopmail.com"))
+Post.create(title: "Singapore", body: "known for Clinliness", user: User.find_by_email("amitabhb@yopmail.com"))
+Post.create(title: "Canada", body: "Good neighbourer of USA", user: User.find_by_email("amits@yopmail.com"))
+Post.create(title: "Tibet", body: "Spiritual Hub", user: User.find_by_email("rajnikantg@yopmail.com"))
+Post.create(title: "Antartica", body: "Known for polar bears", user: User.find_by_email("namo@yopmail.com"))
